56e94015f4bac1b01ebd6ca7a582f2c74404d2a8
Ruy Adorno <ruyadorno@hotmail.com>
2016-08-06 15:35:27 -0400

    Bumped up to v0.1.3

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@ from distutils.core import setup
 setup(
   name = 'telegram-pi-bot',
   packages = ['telegram-pi-bot'],
-  version = '0.1.2',
+  version = '0.1.3',
   description = 'A python-telegram-bot setup to run on Raspberry pi',
   author = 'Ruy Adorno',
   author_email = 'ruyadorno@hotmail.com',
   url = 'https://github.com/ruyadorno/telegram-pi-bot',
-  download_url = 'https://github.com/ruyadorno/telegram-pi-bot/tarball/0.1.2',
+  download_url = 'https://github.com/ruyadorno/telegram-pi-bot/tarball/0.1.3',
   keywords = ['telegram', 'raspberrypi', 'bot', 'python-telegram-bot'],
   install_requires=['python-telegram-bot>=5,<6'],
   classifiers = [],
   entry_points='''
     [console_scripts]
-    telegram-pi-bot=telegram-pi-bot.__init__:main
+    telegram-pi-bot=telegram-pi-bot:main
   '''
 )
 
