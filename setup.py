1fa56d0dee9d20b960f64a03e6af0d99480e4442
Ruy Adorno <ruyadorno@hotmail.com>
2016-08-06 16:56:40 -0400

    Bumped to v0.1.6

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 from distutils.core import setup
 
 setup(
-  name = 'telegram-pi-bot',
-  packages = ['telegram-pi-bot'],
-  version = '0.1.5',
-  description = 'A python-telegram-bot setup to run on Raspberry pi',
-  author = 'Ruy Adorno',
-  author_email = 'ruyadorno@hotmail.com',
-  url = 'https://github.com/ruyadorno/telegram-pi-bot',
-  download_url = 'https://github.com/ruyadorno/telegram-pi-bot/tarball/0.1.5',
-  keywords = ['telegram', 'raspberrypi', 'bot', 'python-telegram-bot'],
-  install_requires=['python-telegram-bot>=5,<6'],
-  classifiers = [],
-  entry_points='''
-    [console_scripts]
-    telegram-pi-bot=telegram-pi-bot.bot:main
-  '''
+    name = 'telegram-pi-bot',
+    packages = ['telegram-pi-bot'],
+    version = '0.1.6',
+    description = 'A python-telegram-bot setup to run on Raspberry pi',
+    author = 'Ruy Adorno',
+    author_email = 'ruyadorno@hotmail.com',
+    url = 'https://github.com/ruyadorno/telegram-pi-bot',
+    download_url = 'https://github.com/ruyadorno/telegram-pi-bot/tarball/0.1.6',
+    keywords = ['telegram', 'raspberrypi', 'bot', 'python-telegram-bot'],
+    install_requires=['python-telegram-bot>=5,<6'],
+    classifiers = [],
+    entry_points = {
+        'console_scripts': [
+            'telegram-pi-bot = telegram-pi-bot.command_line:main'
+        ]
+    }
 )
 
